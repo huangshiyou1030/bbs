07140b0c78ba55cc6898b5feb5ead91d6d1416dc
hsy41802679 <41802679@qq.com>
2018-08-03 08:28:49 +0000

    用户上传头像

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\User;
 use App\Http\Requests\UserRequest;
-use function React\Promise\all;
+use App\Handlers\ImageUploadHandler;
 
 class UsersController extends Controller
 {
@@ -17,10 +17,18 @@ class UsersController extends Controller
     public function edit(User $user){
         return view('users.edit',compact('user'));
     }
-    public function update(UserRequest $request, User $user)
+    public function update(UserRequest $request, ImageUploadHandler $uploader, User $user)
     {
-        $user->update($request->all());
+        $data = $request->all();
+
+        if ($request->avatar) {
+            $result = $uploader->save($request->avatar, 'avatars', $user->id,362);
+            if ($result) {
+                $data['avatar'] = $result['path'];
+            }
+        }
+
+        $user->update($data);
         return redirect()->route('users.show', $user->id)->with('success', '个人资料更新成功！');
     }
-
 }
