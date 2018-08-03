61bb94eca2671fe9f60a7e81a9ab1b430e6b068f
hsy41802679 <41802679@qq.com>
2018-08-03 07:32:14 +0000

    个人资料的修改

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\User;
+use App\Http\Requests\UserRequest;
+use function React\Promise\all;
+
 class UsersController extends Controller
 {
     //
@@ -11,4 +14,13 @@ class UsersController extends Controller
     {
         return view('users.show', compact('user'));
     }
+    public function edit(User $user){
+        return view('users.edit',compact('user'));
+    }
+    public function update(UserRequest $request, User $user)
+    {
+        $user->update($request->all());
+        return redirect()->route('users.show', $user->id)->with('success', '个人资料更新成功！');
+    }
+
 }
