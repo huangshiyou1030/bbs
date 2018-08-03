43b4ba6244085840438b0955dea2ad7bd0532fd6
hsy41802679 <41802679@qq.com>
2018-08-03 03:25:02 +0000

    首页布局

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,10 @@
+
+<footer class="footer">
+    <div class="container">
+        <p class="pull-left">
+由 <a href="http://thinkphp7.com" target="_blank">hsy</a> 设计和编码 <span style="color: #e27575;font-size: 14px;">❤</span>
+        </p>
+
+        <p class="pull-right"><a href="41802679@qq.com">联系我们</a></p>
+    </div>
+</footer>
