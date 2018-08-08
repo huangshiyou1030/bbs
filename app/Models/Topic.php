edb93f42ceddf0ed8aedcb8147c9f3b885964f4d
hsy41802679 <41802679@qq.com>
2018-08-08 07:35:45 +0000

    用户中心添加话题列表

--- app/Models/Topic.php
+++ app/Models/Topic.php
@@ -11,4 +11,33 @@ class Topic extends Model
     public function user(){
         return $this->belongsTo(User::class);
     }
+    public function scopeWithOrder($query,$order){
+        //不同的排序，使用不同的数据读取逻辑
+        switch ($order){
+            case 'recent':
+                $query->recent();
+                break;
+            default:
+                $query->recentReqlied();
+                break;
+        }
+        //预加载防止N+1问题
+        return $query->with('user','category');
+    }
+
+    /**
+     * @return array
+     */
+    public function scoperecentReqlied($query)
+    {
+        //当话题有新回复时，我们将编写逻辑来更新话题模型 的 reply_count 属性，
+        //此时会自动触发框架对数据模型updated_at 时间戳的更新
+        return $query->orderBy('updated_at','desc');
+
+    }
+    public function scopeRecent($query){
+        //按照创建时间排序
+        return $query->orderBy('created_at','desc');
+    }
+
 }
