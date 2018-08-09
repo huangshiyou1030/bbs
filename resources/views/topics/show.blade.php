3887f37e07df9d0f9419a3976e3f63bedc7b0300
hsy41802679 <41802679@qq.com>
2018-08-09 08:01:45 +0000

    用户回复完成

--- resources/views/topics/show.blade.php
+++ resources/views/topics/show.blade.php
@@ -58,8 +58,13 @@
             {{-- 用户回复列表 --}}
             <div class="panel panel-default topic-reply">
                 <div class="panel-body">
-                    @include('topics._reply_box', ['topic' => $topic])
-                    @include('topics._reply_list', ['replies' => $topic->replies()->with('user')->get()])
+                    {{-- 用户回复列表 --}}
+                    <div class="panel panel-default topic-reply">
+                        <div class="panel-body">
+                            @includeWhen(Auth::check(), 'topics._reply_box', ['topic' => $topic])
+                            @include('topics._reply_list', ['replies' => $topic->replies()->with('user')->get()])
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
