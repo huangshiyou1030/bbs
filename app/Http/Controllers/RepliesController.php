4ded4433a46a8f33ad5d0781aaebc73de406a42b
hsy41802679 <41802679@qq.com>
2018-08-09 09:18:38 +0000

    用户删除自己的回复

--- app/Http/Controllers/RepliesController.php
+++ app/Http/Controllers/RepliesController.php
@@ -27,8 +27,10 @@ class RepliesController extends Controller
     public function destroy(Reply $reply)
     {
         $this->authorize('destroy', $reply);
+
+
         $reply->delete();
 
-        return redirect()->route('replies.index')->with('success', '删除成功！');
+        return redirect()->to($reply->topic->link())->with('success', '成功删除回复！');
     }
 }
