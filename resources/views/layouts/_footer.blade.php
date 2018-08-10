2ac9cc56e80fef71f259d359158082210b64111c
hsy41802679 <41802679@qq.com>
2018-08-10 08:03:42 +0000

    站点管理

--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -5,6 +5,6 @@
 由 <a href="http://thinkphp7.com" target="_blank">hsy</a> 设计和编码 <span style="color: #e27575;font-size: 14px;">❤</span>
         </p>
 
-        <p class="pull-right"><a href="41802679@qq.com">联系我们</a></p>
+        <p class="pull-right"><a href="mailto:{{ setting('contact_email') }}">联系我们</a></p>
     </div>
 </footer>
